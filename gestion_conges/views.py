75517a53d11c636c1738d26515e78c62b4c6f980
core.pager=cat <amriameni522@gmail.com>
2024-07-31 00:09:26 +0100

    tache 1

--- gestion_conges/views.py
+++ gestion_conges/views.py
@@ -1,3 +1,4 @@
+# gestion_conges/views.py
 from django.shortcuts import render, redirect
 from .models import Employee, Leave
 from .forms import LeaveRequestForm
@@ -10,6 +11,9 @@ def leave_request(request):
     if request.method == 'POST':
         form = LeaveRequestForm(request.POST, request.FILES)
         if form.is_valid():
+            leave_request = form.save(commit=False)
+            leave_request.employee = Employee.objects.get(user=request.user)
+            leave_request.save()
             form.save()
             return redirect('leave_list')  # Redirection après la soumission
     else:
